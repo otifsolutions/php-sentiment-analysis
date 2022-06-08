859ebade4b3e23f98d02339cdc563aaeb2fea615
Muhammad Ishfaq <m.ishfaq@otif-solutions.com>
2022-06-08 15:08:08 +0500

    README.md updated and Sentiment class converted into anonymous class

--- README.md
+++ README.md
@@ -8,3 +8,36 @@ You can run the following command in the root directory of your project:
 ```
 composer require otifsolutions/php-sentiment-analysis
 ```
+###Usage
+
++ __Php Sentiment Analysis allows user to analyze from a text statement__
++ __Analyze if the statement is positive or negative. e.g. comment on any post.__
+
+   ```php
+   Sentiment::make()->analyze($value);
+   ```
+  - Where $value is the statement to be analyzed.
+    
++  Three possible types of sentiments are labels, emoji, negators.
+   - Labels are letters or words passed in statement e.g. `corruption`, `boycott`.
+   - Emoji contains emojis e.g. `😍`, `💖`.
+   - Negators are words that negates something e.g. `doesnt`, `not`.
+   
++  `getTokens` and `getDataSet` are private methods which have `token` and all three `sentiments`.
+
++  Token can be received from `getTokens` methode. 
++  All other data can be received from `getDataSet` methode. 
+   
+   Following are returned after analyses in an array.
+
+      | Option       |Description                                           |
+      |--------------|---------------------------------------------------|
+      | `score`        |Score after analyze overall statement.    |
+      | `comparative`  |Result score is divided by total number of tokens.   |
+      | `calculation`   |This is value with value stored in dataset, if .              |
+      | `tokens`        |Token is the statement which is passed to analyze if positive or negative.     |
+      | `positive`      |Returns positive value from the statement otherwise nothing is returned.         | 
+      | `negative`      |Returns negative value from the statement otherwise nothing is returned.         | 
+   
+
+
--- src/Sentiment.php
+++ src/Sentiment.php
@@ -6,91 +6,95 @@ namespace OTIFSolutions\PhpSentimentAnalysis;
  * Class Sentiment
  * @package OTIFSolutions\PhpSentimentAnalysis
  */
-class Sentiment
-{
+class Sentiment {
 
     /**
      * @param $value
      * @return array
      */
-    private function getTokens($value){
-        return explode(" ",
-            trim(
-                preg_replace('!\s+!', ' ',
-                    preg_replace("/[^A-Za-z0-9\-]/"," ",
-                        str_replace("\n"," ",
-                            strtolower($value)
+
+    public static function make() {
+        return new class() {
+
+            private function getTokens($value) {
+                return explode(" ",
+                    trim(
+                        preg_replace('!\s+!', ' ',
+                            preg_replace("/[^A-Za-z0-9\-]/", " ",
+                                str_replace("\n", " ",
+                                    strtolower($value)
+                                )
+                            )
                         )
                     )
-                )
-            )
-        );
-    }
+                );
+            }
 
-    /**
-     * @return array
-     */
-    private function getDataSet(){
-        return [
-            'labels' => array_merge(
-                json_decode(file_get_contents(__DIR__ . "/english_labels.json"),true),
-                json_decode(file_get_contents(__DIR__ . "/emoji.json"),true)
-            ),
-            'negators' => json_decode(file_get_contents(__DIR__ . "/negators.json"), true)
-        ];
-    }
+            /**
+             * @return array
+             */
+            private function getDataSet() {
+                return [
+                    'labels' => array_merge(
+                        json_decode(file_get_contents(__DIR__ . "/english_labels.json"), true),
+                        json_decode(file_get_contents(__DIR__ . "/emoji.json"), true)
+                    ),
+                    'negators' => json_decode(file_get_contents(__DIR__ . "/negators.json"), true)
+                ];
+            }
 
-    /**
-     * @param $value
-     * @return false|array
-     */
-    public function analyze($value){
+            /**
+             * @param $value
+             * @return false|array
+             */
+            public function analyze($value) {
 
-        $dataSet = $this->getDataSet();
-        $tokens = $this->getTokens($value);
-        $length = count($tokens);
+                $dataSet = $this->getDataSet();
+                $tokens = $this->getTokens($value);
+                $length = count($tokens);
 
-        $score = 0;
-        $skip = false;
-        $positive = [];
-        $negative = [];
-        $calculation = [];
-        foreach ($tokens as $i => $token){
-            if($skip) {
+                $score = 0;
                 $skip = false;
-                continue;
-            }
-            if(($length-1) > $i){
-                $combine = $token.' '.$tokens[$i+1];
-                if(isset($dataSet['labels'][$combine])){
-                    $tokenScore = $dataSet['labels'][$combine];
-                    $skip = true;
-                    $token = $combine;
-                }
-            }
-            if(!$skip){
-                $tokenScore = isset($dataSet['labels'][$token])? $dataSet['labels'][$token] : 0;
-                if ($i > 0) {
-                    $prevtoken = $tokens[$i - 1];
-                    if (isset($dataSet['negators'][$prevtoken])) {
-                        $tokenScore = -$tokenScore;
+                $positive = [];
+                $negative = [];
+                $calculation = [];
+                foreach ($tokens as $i => $token) {
+                    if ($skip) {
+                        $skip = false;
+                        continue;
                     }
+                    if (($length - 1) > $i) {
+                        $combine = $token . ' ' . $tokens[$i + 1];
+                        if (isset($dataSet['labels'][$combine])) {
+                            $tokenScore = $dataSet['labels'][$combine];
+                            $skip = true;
+                            $token = $combine;
+                        }
+                    }
+                    if (!$skip) {
+                        $tokenScore = isset($dataSet['labels'][$token]) ? $dataSet['labels'][$token] : 0;
+                        if ($i > 0) {
+                            $prevtoken = $tokens[$i - 1];
+                            if (isset($dataSet['negators'][$prevtoken])) {
+                                $tokenScore = -$tokenScore;
+                            }
+                        }
+                    }
+                    if ($tokenScore > 0) $positive[] = $token;
+                    if ($tokenScore < 0) $negative[] = $token;
+                    $score += $tokenScore;
+                    $calculation[] = [$token => $tokenScore];
                 }
+
+                return [
+                    'score' => $score,
+                    'comparative' => sizeof($tokens) > 0 ? $score / sizeof($tokens) : 0,
+                    'calculation' => $calculation,
+                    'tokens' => $tokens,
+                    'positive' => $positive,
+                    'negative' => $negative
+                ];
             }
-            if ($tokenScore > 0) $positive[] = $token;
-            if ($tokenScore < 0) $negative[] = $token;
-            $score += $tokenScore;
-            $calculation[] = [ $token => $tokenScore ];
-        }
-
-        return [
-            'score' => $score,
-            'comparative' => sizeof($tokens) > 0 ? $score / sizeof($tokens) : 0,
-            'calculation' => $calculation,
-            'tokens' => $tokens,
-            'positive' => $positive,
-            'negative' => $negative
-        ];
+        };
     }
-
 }
